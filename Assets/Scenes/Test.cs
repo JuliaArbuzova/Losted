e9feb31017ea01627e2e3912d671f0e4ac873fba
Julia <yuaarbuzova@edu.hse.ru>
2024-03-15 20:19:38 +0300

    Add levels

--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -1,33 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-  private void Update()
-  {
-    if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(-5 * Time.deltaTime, 0, 0);
-
-    if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(5 * Time.deltaTime, 0, 0);
-
-    if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(0, 5 * Time.deltaTime, 0);
-
-    if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(0, -5 * Time.deltaTime, 0);
-  }
-
-  private void OnCollisionEnter2D(Collision2D collision)
-  {
-    if (collision.gameObject.CompareTag($"Walls"))
+    // Start is called before the first frame update
+    private void Awake()
     {
-      if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(5 * Time.deltaTime, 0, 0);
-
-      if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(-5 * Time.deltaTime, 0, 0);
-
-      if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(0, -5 * Time.deltaTime, 0);
-
-      if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(0, 5 * Time.deltaTime, 0);
     }
-    //if (collision.gameObject.tag == "Exit")
-    //{
-    //
-    //}
-  }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-5 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(5 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 5 * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -5 * Time.deltaTime, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Walls")
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(5 * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(-5 * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(0, -5 * Time.deltaTime, 0);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(0, 5 * Time.deltaTime, 0);
+            }
+        }
+        //if (collision.gameObject.tag == "Exit")
+        //{
+        //
+        //}
+    }
+    
 }
