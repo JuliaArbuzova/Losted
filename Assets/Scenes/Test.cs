e1fc0720593a409cabe1d76c6abbc91d1c897cc0
Daniil Makagonov <daniil1makagonov@gmail.com>
2024-03-15 18:14:53 +0300

    Clean code style

--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -1,66 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private void Awake()
+  private void Update()
+  {
+    if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(-5 * Time.deltaTime, 0, 0);
+
+    if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(5 * Time.deltaTime, 0, 0);
+
+    if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(0, 5 * Time.deltaTime, 0);
+
+    if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(0, -5 * Time.deltaTime, 0);
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.CompareTag($"Walls"))
     {
+      if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(5 * Time.deltaTime, 0, 0);
+
+      if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(-5 * Time.deltaTime, 0, 0);
+
+      if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(0, -5 * Time.deltaTime, 0);
+
+      if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(0, 5 * Time.deltaTime, 0);
     }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(-5 * Time.deltaTime, 0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(5 * Time.deltaTime, 0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(0, 5 * Time.deltaTime, 0);
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(0, -5 * Time.deltaTime, 0);
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Walls")
-        {
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.Translate(5 * Time.deltaTime, 0, 0);
-            }
-
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.Translate(-5 * Time.deltaTime, 0, 0);
-            }
-
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.Translate(0, -5 * Time.deltaTime, 0);
-            }
-
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.Translate(0, 5 * Time.deltaTime, 0);
-            }
-        }
-        //if (collision.gameObject.tag == "Exit")
-        //{
-        //
-        //}
-    }
-    
+    //if (collision.gameObject.tag == "Exit")
+    //{
+    //
+    //}
+  }
 }
