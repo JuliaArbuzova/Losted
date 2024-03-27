7007a39d9b773d73bbaaa255144e39996da792a6
Julia <yuaarbuzova@edu.hse.ru>
2024-03-27 19:27:18 +0300

    First steps for pause

---
+++ Assets/Scripts/Pause_Menu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Menu : MonoBehaviour
+{
+    public bool PausedGame;
+    public GameObject
+    private void Update()
+    {
+        
+    }
+}
